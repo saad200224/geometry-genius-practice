163dea1290ae8edb302e75afab6c78537b9d5dca
saad200224 <saad19021994@outlook.com>
2023-08-19 12:36:59 +0600

    first

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -6,6 +6,24 @@ function calculateTriangleArea(){
     const heightField = document.getElementById('triangle-height');
     const heightValueText = heightField.value;
     const height = parseFloat(heightValueText);
-    console.log(height)
 
+    const area = 0.5 * base * height;
+
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
 }
+
+    function calculateRectangleArea(){
+        const widthField = document.getElementById('rectangle-width');
+        const widthValueText = widthField.value;
+        const width = parseFloat(widthValueText);
+
+        const lengthField = document.getElementById('rectangle-length');
+        const lengthValueText = lengthField.value;
+        const length = parseFloat(lengthValueText);
+
+        const area = width * length;
+
+        const areaSpan = document.getElementById('rectangle-area');
+        areaSpan.innerText = area;
+    }
